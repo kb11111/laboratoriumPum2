c025a593f324b73690318e6546d0f2747de3c689
kb11111 <kbielecka646@gmail.com>
2017-12-13 15:27:21 +0100

    Czesc kodu

--- app/src/main/java/com/example/student/appkapogodowa/MainActivity.java
+++ app/src/main/java/com/example/student/appkapogodowa/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.student.appkapogodowa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText zmien = findViewById(R.id.Et_WpiszMiasto);
+        Button przycisk = findViewById(R.id.btn_zmienMiejsce);
+        TextView miasto =  findViewById(R.id.txtv_NazwaMiasta);
+
+
+        przycisk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            
+            }
+        });
+
     }
+
 }
