b1160eadbe4068c14deb8f4c9b5df9d9870964e8
kb11111 <kbielecka646@gmail.com>
2017-12-13 15:29:36 +0100

    zmiany

--- app/src/main/java/com/example/student/appkapogodowa/MainActivity.java
+++ app/src/main/java/com/example/student/appkapogodowa/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         przycisk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            
+
             }
         });
 
