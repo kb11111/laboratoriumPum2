dcf209e1fdfcef1c3a0a214f3513f096560f7035
kb11111 <kbielecka646@gmail.com>
2017-12-20 15:19:00 +0100

    Butterknife

--- app/src/main/java/com/example/student/appkapogodowa/MainActivity.java
+++ app/src/main/java/com/example/student/appkapogodowa/MainActivity.java
@@ -8,38 +8,74 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.Et_WpiszMiasto)
+    EditText Et_WpiszMiasto ;
+
+    @BindView(R.id.btn_zmienMiejsce)
+    Button btn_zmienMiejsce;
+
+    @BindView(R.id.btn_lista)
+    Button btn_lista;
+
+    @BindView(R.id.txtv_NazwaMiasta)
+    TextView   txtv_NazwaMiasta;
+
+    @OnClick(R.id.Et_WpiszMiasto)
+    void onClick(View view){
+
+            String nowe = Et_WpiszMiasto.getText().toString();
+        txtv_NazwaMiasta.setText(nowe);
+    }
+
+    @OnClick(R.id.btn_lista)
+    void onClick2(View view){
+
+        Intent intent = new Intent( MainActivity.this, Main2Activity.class);
+        startActivity(intent);
+        Bundle bundle = new Bundle();
+        bundle.putString("zmien","miasto");
+        intent.putExtras(bundle);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final EditText zmien = findViewById(R.id.Et_WpiszMiasto);
-        final Button przycisk = findViewById(R.id.btn_zmienMiejsce);
-        Button lista = findViewById(R.id.btn_lista);
-        final TextView miasto =  findViewById(R.id.txtv_NazwaMiasta);
+        ButterKnife.bind(this);
 
 
-        przycisk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            String nowe = zmien.getText().toString();
-            miasto.setText(nowe);
-            }
-        });
 
-       lista.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               Intent intent = new Intent( MainActivity.this, Main2Activity.class);
-               startActivity(intent);
-               Bundle bundle = new Bundle();
-               bundle.putString("zmien","miasto");
-               intent.putExtras(bundle);
+        //final Button przycisk =  @BindView(R.id.btn_zmienMiejsce);
+       // Button lista =  @BindView(R.id.btn_lista);
+        //final TextView miasto =   @BindView(R.id.txtv_NazwaMiasta);
 
-           }
-       });
+
+        //przycisk.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+           // public void onClick(View view) {
+           // String nowe = zmien.getText().toString();
+            //miasto.setText(nowe);
+            //}
+       // });
+
+      // lista.setOnClickListener(new View.OnClickListener() {
+        //   @Override
+        //   public void onClick(View view) {
+          //     Intent intent = new Intent( MainActivity.this, Main2Activity.class);
+           //    startActivity(intent);
+            //   Bundle bundle = new Bundle();
+             //  bundle.putString("zmien","miasto");
+           //    intent.putExtras(bundle);
+
+          // }
+       //});
 
 
 
